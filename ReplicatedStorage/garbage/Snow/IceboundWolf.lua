46f8ab1b24d0c10da4be63ae053d4a0dfa0d4622
RMcCobb <ricky.mccobb@gmail.com>
2026-05-14 16:20:31 -0700

    Updated ReplicatedStorage/garbage/Snow/IceboundWolf.lua

---
+++ ReplicatedStorage/garbage/Snow/IceboundWolf.lua
@@ -0,0 +1,11 @@
+-- @ScriptType: ModuleScript
+-- create a Water card object
+local IceboundWolf = {
+	id = "c10",
+	name = "Icebound Wolf",
+	element = "snow",
+	value = 4,
+	weight = 30
+}
+
+return IceboundWolf
