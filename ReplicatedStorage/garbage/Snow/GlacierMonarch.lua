503b6e817cda852a2785b5a11f8113c07c63e859
RMcCobb <ricky.mccobb@gmail.com>
2026-05-14 16:20:18 -0700

    Updated ReplicatedStorage/garbage/Snow/GlacierMonarch.lua

---
+++ ReplicatedStorage/garbage/Snow/GlacierMonarch.lua
@@ -0,0 +1,11 @@
+-- @ScriptType: ModuleScript
+-- create a Water card object
+local GlacierMonarch = {
+	id = "c9",
+	name = "Glacier Monarch",
+	element = "snow",
+	value = 8,
+	weight = 7
+}
+
+return GlacierMonarch
