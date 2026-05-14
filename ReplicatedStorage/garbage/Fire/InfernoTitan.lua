047d7c8e895f87b88bf271457e6e726eeb796c6d
RMcCobb <ricky.mccobb@gmail.com>
2026-05-14 16:20:22 -0700

    Updated ReplicatedStorage/garbage/Fire/InfernoTitan.lua

---
+++ ReplicatedStorage/garbage/Fire/InfernoTitan.lua
@@ -0,0 +1,12 @@
+-- @ScriptType: ModuleScript
+-- create a fire card object
+local InfernoTitan = {
+	id = "c4",
+	name = "Inferno Titan",
+	element = "fire",
+	value = "8",
+	weight = 10
+}
+
+
+return InfernoTitan
