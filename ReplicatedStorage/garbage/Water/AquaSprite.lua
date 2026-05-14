6d9e497b18a0775494d6f59983579619b162de27
RMcCobb <ricky.mccobb@gmail.com>
2026-05-14 16:20:49 -0700

    Updated ReplicatedStorage/garbage/Water/AquaSprite.lua

---
+++ ReplicatedStorage/garbage/Water/AquaSprite.lua
@@ -0,0 +1,12 @@
+-- @ScriptType: ModuleScript
+-- create a fire card object
+local AquaSprite = {
+	id = "c12",
+	name = "Aqua Sprite",
+	element = "water",
+	value = 2,
+	weight = .45
+}
+
+
+return AquaSprite
