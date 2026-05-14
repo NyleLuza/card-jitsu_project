d4b6323db6bb04969f27d15443b22b9246637321
RMcCobb <ricky.mccobb@gmail.com>
2026-05-14 16:21:02 -0700

    Updated ReplicatedStorage/garbage/Water/MistGuardian.lua

---
+++ ReplicatedStorage/garbage/Water/MistGuardian.lua
@@ -0,0 +1,11 @@
+-- @ScriptType: ModuleScript
+-- create a fire card object
+local MistGuardian = {
+	id = "c13",
+	name = "Mist Guardian",
+	element = "water",
+	value = 5,
+	weight = .15
+}
+
+return MistGuardian
