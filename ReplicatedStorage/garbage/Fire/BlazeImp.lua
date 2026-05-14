83f986a52d01d379d580cee62106f99decaae339
RMcCobb <ricky.mccobb@gmail.com>
2026-05-14 16:20:51 -0700

    Updated ReplicatedStorage/garbage/Fire/BlazeImp.lua

---
+++ ReplicatedStorage/garbage/Fire/BlazeImp.lua
@@ -0,0 +1,12 @@
+-- @ScriptType: ModuleScript
+-- create a fire card object
+local BlazeImp = {
+	id = "c1",
+	name = "Blaze Imp",
+	element = "fire",
+	value = 3,
+	weight = 40
+}
+
+
+return BlazeImp
