38ec7438213714a166999290f82940e4c9b87423
RMcCobb <ricky.mccobb@gmail.com>
2026-05-14 16:20:43 -0700

    Updated ReplicatedStorage/garbage/Snow/Frostling.lua

---
+++ ReplicatedStorage/garbage/Snow/Frostling.lua
@@ -0,0 +1,11 @@
+-- @ScriptType: ModuleScript
+-- create a Water card object
+local Frostling = {
+	id = "c8",	
+	name = "Frostling",
+	element = "snow",
+	value = 1,
+	weight = 50
+}
+
+return Frostling
