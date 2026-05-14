322d8ea43adcfb9ccff84393c282c20911ffaecd
RMcCobb <ricky.mccobb@gmail.com>
2026-05-14 16:20:15 -0700

    Updated ReplicatedStorage/garbage/Water/TidalSerpent.lua

---
+++ ReplicatedStorage/garbage/Water/TidalSerpent.lua
@@ -0,0 +1,11 @@
+-- @ScriptType: ModuleScript
+-- create a fire card object
+local TidalSerpent = {
+	id = "c14",
+	name = "Tidal Serpent",
+	element = "water",
+	value = 7,
+	weight = .10
+}
+
+return TidalSerpent
