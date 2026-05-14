221b5063c9dc77f7f75f537f09a1fce5ae9d3dc5
RMcCobb <ricky.mccobb@gmail.com>
2026-05-14 16:20:13 -0700

    Updated ReplicatedStorage/garbage/Fire/EmberSpirit.lua

---
+++ ReplicatedStorage/garbage/Fire/EmberSpirit.lua
@@ -0,0 +1,12 @@
+-- @ScriptType: ModuleScript
+-- create a fire card object
+local EmberSpirit = {
+	id = "c2",
+	name = "Ember Spirit",
+	element = "fire",
+	value = 5,
+	weight = 20
+}
+
+
+return EmberSpirit
