f85fc2dc7e2ff7339d395a14f123e52ace1a1b65
RMcCobb <ricky.mccobb@gmail.com>
2026-05-14 16:20:19 -0700

    Updated ReplicatedStorage/garbage/Fire/FlameWarden.lua

---
+++ ReplicatedStorage/garbage/Fire/FlameWarden.lua
@@ -0,0 +1,12 @@
+-- @ScriptType: ModuleScript
+-- create a fire card object
+local FlameWarden = {
+	id = "c3",
+	name = "Flame Warden",
+	element = "fire",
+	value = 6,
+	weight = 25
+}
+
+
+return FlameWarden
