8e3d89e9e707af623dc385e327141c18ea126c1c
RMcCobb <ricky.mccobb@gmail.com>
2026-05-14 16:20:55 -0700

    Updated ReplicatedStorage/garbage/Series1/Series1.lua

---
+++ ReplicatedStorage/garbage/Series1/Series1.lua
@@ -0,0 +1,28 @@
+-- @ScriptType: ModuleScript
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local CardFolder = ReplicatedStorage:WaitForChild("Cards")
+
+local FireSet = CardFolder:WaitForChild("Fire")
+local SnowSet = CardFolder:WaitForChild("Snow")
+local WaterSet = CardFolder:WaitForChild("Water")
+
+
+local Pack1 = {
+	id = "p1",
+	name = "Origins Pack",
+	description = "A balanced pack containing Fire, Water, and Snow cards.",
+	image = "temp",
+	cost = 100,
+	num_cards = 6,
+	cards = {
+		require(FireSet:WaitForChild("BlazeImp")),
+		require(FireSet:WaitForChild("PyroDrake")),
+		require(SnowSet:WaitForChild("Frostling")),
+		require(SnowSet:WaitForChild("CrystalGolem")),
+		require(WaterSet:WaitForChild("AquaSprite")),
+		require(WaterSet:WaitForChild("AbyssLeviathan")),
+	}
+}
+
+
+return Pack1
