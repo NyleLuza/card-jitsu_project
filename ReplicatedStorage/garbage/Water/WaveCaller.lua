a95ad72517f7f280130cde82b17f5b2e7ee0397b
RMcCobb <ricky.mccobb@gmail.com>
2026-05-14 16:20:12 -0700

    Updated ReplicatedStorage/garbage/Water/WaveCaller.lua

---
+++ ReplicatedStorage/garbage/Water/WaveCaller.lua
@@ -0,0 +1,11 @@
+-- @ScriptType: ModuleScript
+-- create a fire card object
+local WaveCaller = {
+	id = "c15",
+	name = "Wavecaller",
+	element = "water",
+	value = 4,
+	weight = .25
+}
+
+return WaveCaller
