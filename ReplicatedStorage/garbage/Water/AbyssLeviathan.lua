d71accd5afac7062a3b32f3decd4e0a469da2c5c
RMcCobb <ricky.mccobb@gmail.com>
2026-05-14 16:20:41 -0700

    Updated ReplicatedStorage/garbage/Water/AbyssLeviathan.lua

---
+++ ReplicatedStorage/garbage/Water/AbyssLeviathan.lua
@@ -0,0 +1,11 @@
+-- @ScriptType: ModuleScript
+-- create a fire card object
+local AbyssLeviathan = {
+	id = "c11",
+	name = "Abyss Leviathan",
+	element = "water",
+	value = 10,
+	weight = .05
+}
+
+return AbyssLeviathan
