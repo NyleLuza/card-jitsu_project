5ea7dc36c814b27d7b77ddbd0920898c872c923c
RMcCobb <ricky.mccobb@gmail.com>
2026-05-14 16:20:45 -0700

    Updated ReplicatedStorage/garbage/Snow/BlizzardPhantom.lua

---
+++ ReplicatedStorage/garbage/Snow/BlizzardPhantom.lua
@@ -0,0 +1,11 @@
+-- @ScriptType: ModuleScript
+-- create a Water card object
+local BlizzardPhantom = {
+	id = "c6",
+	name = "Blizzard Phantom",
+	element = "snow",
+	value = 9,
+	weight = 1
+}
+
+return BlizzardPhantom
