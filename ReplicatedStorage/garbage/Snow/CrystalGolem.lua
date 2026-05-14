914c61df5ce4648dae5308871280ab140ab05b29
RMcCobb <ricky.mccobb@gmail.com>
2026-05-14 16:20:42 -0700

    Updated ReplicatedStorage/garbage/Snow/CrystalGolem.lua

---
+++ ReplicatedStorage/garbage/Snow/CrystalGolem.lua
@@ -0,0 +1,11 @@
+-- @ScriptType: ModuleScript
+-- create a Water card object
+local CrystalGolem = {
+	id = "c7",
+	name = "Crystal Golem",
+	element = "snow",
+	value = 6,
+	weight = 10
+}
+
+return CrystalGolem
