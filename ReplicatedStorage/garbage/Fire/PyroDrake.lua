18f4a9abe7fb423683393888c7b86fcb40bdec2d
RMcCobb <ricky.mccobb@gmail.com>
2026-05-14 16:20:25 -0700

    Updated ReplicatedStorage/garbage/Fire/PyroDrake.lua

---
+++ ReplicatedStorage/garbage/Fire/PyroDrake.lua
@@ -0,0 +1,12 @@
+-- @ScriptType: ModuleScript
+-- create a fire card object
+local PyroDrake = {
+	id = "c5",
+	name = "Pyro Drake",
+	element = "fire",
+	value = 9,
+	weight = 5
+}
+
+
+return PyroDrake
