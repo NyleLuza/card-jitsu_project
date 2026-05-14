cbf5bcfc77bc52652a3c0e0ddfc7c8de9f1ed28a
RMcCobb <ricky.mccobb@gmail.com>
2026-05-14 16:21:07 -0700

    Updated ReplicatedStorage/Cards/CardConfig.lua

---
+++ ReplicatedStorage/Cards/CardConfig.lua
@@ -0,0 +1,104 @@
+-- @ScriptType: ModuleScript
+local CardConfig = {
+	-- fire
+	BlazeImp = {
+		id = "c1",
+		name = "Blaze Imp",
+		element = "fire",
+		value = 3,
+		weight = 40
+	}, EmberSpirit = {
+			id = "c2",
+			name = "Ember Spirit",
+			element = "fire",
+			value = 5,
+			weight = 20
+		}, FlameWarden = {
+			id = "c3",
+			name = "Flame Warden",
+			element = "fire",
+			value = 6,
+			weight = 25
+		}, InfernoTitan = {
+			id = "c4",
+			name = "Inferno Titan",
+			element = "fire",
+			value = 8,
+			weight = 10
+		},PyroDrake = {
+			id = "c5",
+			name = "Pyro Drake",
+			element = "fire",
+			value = 9,
+			weight = 5
+		},
+		-- fire
+		-- snow
+	BlizzardPhantom = {
+		id = "c6",
+		name = "Blizzard Phantom",
+		element = "snow",
+		value = 9,
+		weight = 1
+	}, CrystalGolem = {
+			id = "c7",
+			name = "Crystal Golem",
+			element = "snow",
+			value = 6,
+			weight = 10
+		}, Frostling = {
+			id = "c8",	
+			name = "Frostling channel",
+			element = "snow",
+			value = 1,
+			weight = 50
+		}, GlacierMonarch = {
+			id = "c9",
+			name = "Glacier Monarch",
+			element = "snow",
+			value = 8,
+			weight = 7
+		}, IceboundWolf = {
+			id = "c10",
+			name = "Icebound Wolf",
+			element = "snow",
+			value = 4,
+			weight = 30
+		},
+		-- snow
+		-- water
+	AbyssLeviathan = {
+		id = "c11",
+		name = "Abyss Leviathan",
+		element = "water",
+		value = 10,
+		weight = 05
+	}, AquaSprite = {
+			id = "c12",
+			name = "Aqua Sprite",
+			element = "water",
+			value = 2,
+			weight = 45
+		}, MistGuardian = {
+			id = "c13",
+			name = "Mist Guardian",
+			element = "water",
+			value = 5,
+			weight = 15
+		},TidalSerpent = {
+			id = "c14",
+			name = "Tidal Serpent",
+			element = "water",
+			value = 7,
+			weight = 10
+		},WaveCaller = {
+			id = "c15",
+			name = "Wavecaller",
+			element = "water",
+			value = 4,
+			weight = 25
+		}
+		-- water
+}
+
+return CardConfig
