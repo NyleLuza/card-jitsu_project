0eb5ef1dbf9c7c625c99bcf3d42355f36ef26e38
RMcCobb <ricky.mccobb@gmail.com>
2026-05-14 18:57:55 -0700

    Updated StarterPlayer/StarterPlayerScripts/ShopUIHandler.lua

--- StarterPlayer/StarterPlayerScripts/ShopUIHandler.lua
+++ StarterPlayer/StarterPlayerScripts/ShopUIHandler.lua
@@ -1,40 +1,12 @@
 -- @ScriptType: LocalScript
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ShopEvent = ReplicatedStorage:WaitForChild("RemoteEvents"):WaitForChild("ShopAction")
-local ShopUtils = require(ReplicatedStorage:WaitForChild("Util"):WaitForChild("ShopUtils"))
-local Shop = nil
-local UIS = game:GetService("UserInputService")
-local rs = game:GetService("RunService")
-
--- init perspective
--- UIS.MouseBehavior = Enum.MouseBehavior.LockCenter
--- UIS.MouseIconEnabled = false
-
--- controls camera perspective
-local function UpdateMouse()
-	-- if Shop == nil then
-	-- 	UIS.MouseBehavior = Enum.MouseBehavior.LockCenter
-	-- 	UIS.MouseIconEnabled = false
-	-- else
-		UIS.MouseBehavior = Enum.MouseBehavior.Default
-		UIS.MouseIconEnabled = true
-	-- end
-end
+local ClickUtils = require(ReplicatedStorage:WaitForChild("Util"):WaitForChild("ClickUtils"))
 
 ShopEvent.OnClientEvent:Connect(function(...)
-	-- checks to make sure there is only one instance of a shop
-	if Shop == nil then
-		Shop = ShopUtils.OpenShop()
-		UpdateMouse()
-	end
-	
-	-- exit function
+	local Shop = ClickUtils.OpenShop()
 	local exit = Shop:WaitForChild("MainFrame"):WaitForChild("ExitButton")
 	exit.MouseButton1Click:Connect(function()
-	ShopUtils.CloseShop(Shop)
-	
-	-- update Shop status after closing
-	Shop = nil
-	UpdateMouse()
+	ClickUtils.CloseShop(Shop)
 	end)
 end)
